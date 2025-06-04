31332be3b90605378178186ccb730982d78fa106
郝一平 <14609765+hao-yiping@user.noreply.gitee.com>
2025-06-04 16:31:10 +0800

    test.cpp

--- test/main.cpp
+++ test/main.cpp
@@ -15,8 +15,45 @@ int main(int argc, char* argv[])
 	info = test_entrance(output_path);
 	return info;
 }
+class CFile
+{
+public:
+    CFile();
+    ~CFile();
+    FILE* Open(const char* name, const char* mode);
+    FILE* OpenRead(const char* s);//SH_read_from_file__(const char* s);
+    FILE* OpenWriteAtRear(const char* s);//fp_write_at_rear(const char* s);
+    FILE* OpenWritePlus(const char* s);//fp_write_plus
+    std::string FusePathAndName(const std::string& path, const std::string& name);//path_add_file_name
+    std::string FusePathAndName(const char* path, const std::string& name);//path_add_file_name
+    std::string ChangeSuffix(const std::string& file, const char* new_one);//string suffix_change(const string& file, const char* new_one);
+private:
+
+};
 int static entrance(int argc, char* argv[])
 {
+    hyperlex::dictionary para;
+    FILE* fp;
+    std::string file;
+    CFile CF;
+    int error;
+    hyperlex::BufferChar input;
+    hyperlex::BufferChar temp;
+
+    file = "./parameter/para.txt";
+    std::cout << "+++++++++++++++++++++++++++++++++++++++++++++" << std::endl;
+    fp = CF.OpenRead(file.c_str());
+    std::cout << "InputFileName: " << file << std::endl;
+    input << fp;
+    temp.append(input);
+    std::cout << "/*" << std::endl;
+    std::cout << temp.ptr() << std::endl;
+    std::cout << "*/" << std::endl;
+    std::cout << "+++++++++++++++++++++++++++++++++++++++++++++" << std::endl;
+    fclose(fp);
+    error = para.build(input.ptr());
+    std::cout << "error: " << error << std::endl;
+    para.print(stdout);
 
 }
 
@@ -25,4 +62,73 @@ int static entrance(int argc, char* argv[])
 
 
 
+
+FILE* CFile::Open(const char* name, const char* mode)
+{
+    FILE* fp;
+    int error;
+    errno = 0;
+#ifdef  _WIN64
+    error = fopen_s(&fp, name, mode);
+    if (error != 0)
+    {
+        fprintf(stderr, "WARNING: FAILURE OPEN:%s, errno_t:%d\n", name, error);
+        fp = NULL;
+    }
+#else
+    fp = fopen(name, mode);
+    if (fp == NULL)
+        fprintf(stderr, "WARNING: FAILURE OPEN:%s, Error no.%d: %s\n", name, errno, strerror(errno));
+#endif
+    ExitWarning((fp == NULL), stderr);
+    return fp;
+}
+FILE* CFile::OpenRead(const char* name)
+{
+    return Open(name, "r");
+}
+FILE* CFile::OpenWriteAtRear(const char* name)
+{
+    return Open(name, "a+");
+}
+FILE* CFile::OpenWritePlus(const char* name)
+{
+    return Open(name, "w+");
+}
+std::string CFile::FusePathAndName(const std::string& path, const std::string& name)
+{
+    std::string fusion;
+    if (path[path.length() - 1] == '/')
+        fusion = path + name;
+    else
+        fusion = path + '/' + name;
+    return fusion;
+}
+std::string CFile::FusePathAndName(const char* path, const std::string& name)
+{
+    std::string PPP;
+    PPP = path;
+    return FusePathAndName(PPP, name);
+}
+std::string CFile::ChangeSuffix(const std::string& file, const char* new_one)
+{
+    size_t i, j;
+    std::string name;
+    name = "";
+    for (i = file.length(); i != 0; i--)
+        if (file[i - 1] == '.') break;
+    if (i == 0)
+    {
+        name = file;
+        name += '.';
+    }
+    else
+    {
+        for (j = 0; j < i; j++)
+            name += file[j];
+    }
+    if (new_one[0] == '.') name += (new_one + 1);
+    else name += new_one;
+    return name;
+}
 
